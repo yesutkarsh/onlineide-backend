f90f873c9ba2110b9187ce6fe8c0e61b01ba0208
yesutkarsh <contactutkarshverma@gmail.com>
2024-09-08 01:04:54 +0530

    second

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Server is running on port 4000');
 });
